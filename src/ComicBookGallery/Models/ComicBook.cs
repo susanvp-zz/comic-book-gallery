3b881cc87a2c4ef31a11ac2adc9c43366531d3a6
susan <susan@infi.nl>
2017-09-29 10:26:40 +0200

    Updated comic book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -13,5 +13,10 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText => SeriesTitle + " #" + IssueNumber;
+
+        // series-title-issuenumber.jpg
+        public string CoverImageFileName => SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + "jpg";
     }
 }
